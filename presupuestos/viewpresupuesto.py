dd63b7a72bc0ceabc444d9d3ffefc1279cfd6f85
mar-ale-fer <mar.ale.fer@gmail.com>
2015-10-01 19:26:45 -0300

    fecha en presupuestos y ajuste en templates

--- presupuestos/viewpresupuesto.py
+++ presupuestos/viewpresupuesto.py
@@ -1,8 +1,10 @@
  # -- coding: utf-8 --
+from django import forms
 from django.core.urlresolvers import reverse, reverse_lazy
+from django.forms.extras.widgets import SelectDateWidget
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 presupuesto_fields = ('cliente','referencia', 'tipo', 'fecha_de_solicitud', 'fecha_de_aprobacion', 'descripcion', 'estado', 'observacion')
-
+fecha_solicitud_anios = ('2015', '2016', '2017')
 from .models import Presupuesto
 
 #Presupuesto
@@ -10,10 +12,30 @@ class PresupuestoListar(ListView):
     model = Presupuesto
 	#context_object_name = 'lista_de_presupuestos' #opcion a object_list
 
+#class CrearForm(forms.ModelForm):
+#    class Meta:
+#        model = Presupuesto
+#        fields = ['cliente','referencia','fecha_de_solicitud'] #presupuesto_fields
+#        #fields[2].widget = forms.DateField(widget=SelectDateWidget(empty_label="Nothing"))
+
+class PresupuestoForm(forms.ModelForm):
+    #cliente lo uso por defecto
+    #referencia = forms.CharField(max_length=128, help_text="Por favor ingrese el nombre de la categoría")
+    fecha_de_solicitud = forms.DateField(widget=SelectDateWidget(empty_label="Nothing"))
+    fecha_de_aprobacion = forms.DateField(widget=SelectDateWidget(empty_label="Nothing"))
+	#Una clase inline para proveer información adicional en el formulario
+    class Meta:
+        #Provee una asociación entre el ModelForm y un model
+        model = Presupuesto
+        fields = presupuesto_fields	
+		
+		
 class PresupuestoCrear(CreateView):
     model = Presupuesto
     success_url = reverse_lazy('presupuestos:presupuesto_listar')
-    fields = presupuesto_fields
+    form_class = PresupuestoForm
+
+	
 
 class PresupuestoDetalle(DetailView):
     model = Presupuesto
@@ -21,7 +43,8 @@ class PresupuestoDetalle(DetailView):
 
 class PresupuestoModificar(UpdateView):
     model = Presupuesto
-    fields = presupuesto_fields
+    #fields = presupuesto_fields
+    form_class = PresupuestoForm
 	
     def get_success_url(self):
         return reverse('presupuestos:presupuesto_detalle', kwargs={
