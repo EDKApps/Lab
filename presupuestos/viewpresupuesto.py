fbe06c15ffc4efc3e5068413e30ebd118d851b0a
mar-ale-fer <mar.ale.fer@gmail.com>
2015-10-26 04:50:46 -0300

    filtro en presupuestos

--- presupuestos/viewpresupuesto.py
+++ presupuestos/viewpresupuesto.py
@@ -3,6 +3,7 @@ from django import forms
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.forms.extras.widgets import SelectDateWidget
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django.db.models import Q #para OR en consultas
 #En el form de alta excluyo la referencia (automática)
 presupuesto_fields_crear = ('cliente','referencia_clave', 'tipo', 'fecha_de_solicitud',
                       'fecha_de_aprobacion','fecha_de_vencimiento', 'descripcion', 'estado', 'observacion')
@@ -17,7 +18,24 @@ class PresupuestoListar(ListView):
     model = Presupuesto
     paginate_by = 5
 	#context_object_name = 'lista_de_presupuestos' #opcion a object_list
-
+    #búsqueda
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query is None:
+            return Presupuesto.objects.all()
+        else:
+            return Presupuesto.objects.filter( Q(referencia_clave__icontains=query) | 
+                                           Q(referencia__icontains=query) |
+                                           Q(cliente__empresa__icontains=query) )
+    #almacenar contexto de la búsqueda
+    def get_context_data(self, **kwargs):
+        context = super(PresupuestoListar, self).get_context_data(**kwargs)
+        q = self.request.GET.get('q')
+        if q: #si existe el valor, lo agrego/actualizo en el contexto
+            q = q.replace(" ","+")
+            context['query'] = q
+        return context    
+	
 class PresupuestoFormCrear(forms.ModelForm):
     class Meta:
         #Provee una asociación entre el ModelForm y un model
